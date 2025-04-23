1668b9ba2230f84365e6dd97dc1c730c379806de
eunoia73 <soo97703@naver.com>
2025-04-23 14:21:15 +0900

    refactor: #3 Receiver, Address 값 객체화

--- src/main/java/com/example/ddd/domain/order/ShippingInfo.java
+++ src/main/java/com/example/ddd/domain/order/ShippingInfo.java
@@ -1,38 +1,14 @@
 package com.example.ddd.domain.order;
 
 public class ShippingInfo {
-    private String receiverName;
-    private String receiverPhoneNumber;
-    private String ShippingAddress1;
-    private String ShippingAddress2;
-    private String shippingZipCode;
+    private Receiver receiver;
+    private Address address;
 
-    public ShippingInfo(String receiverName, String receiverPhoneNumber, String shippingAddress1, String shippingAddress2, String shippingZipCode) {
-        this.receiverName = receiverName;
-        this.receiverPhoneNumber = receiverPhoneNumber;
-        ShippingAddress1 = shippingAddress1;
-        ShippingAddress2 = shippingAddress2;
-        this.shippingZipCode = shippingZipCode;
+    public Receiver getReceiver() {
+        return receiver;
     }
 
-    public String getReceiverName() {
-        return receiverName;
+    public Address getAddress() {
+        return address;
     }
-
-    public String getReceiverPhoneNumber() {
-        return receiverPhoneNumber;
-    }
-
-    public String getShippingAddress1() {
-        return ShippingAddress1;
-    }
-
-    public String getShippingAddress2() {
-        return ShippingAddress2;
-    }
-
-    public String getShippingZipCode() {
-        return shippingZipCode;
-    }
-
 }
