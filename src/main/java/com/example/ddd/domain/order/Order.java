b585a0534177942e1fb5893f340b7b485e68e89b
eunoia73 <soo97703@naver.com>
2025-04-23 14:17:33 +0900

    feat: #3 Order 엔티티화(식별자 orderNumber)

--- src/main/java/com/example/ddd/domain/order/Order.java
+++ src/main/java/com/example/ddd/domain/order/Order.java
@@ -3,10 +3,28 @@ package com.example.ddd.domain.order;
 import java.util.List;
 
 public class Order {
+    private String orderNumber;
     private OrderState state;
     private ShippingInfo shippingInfo;
     private List<OrderLine> orderLines;
     private Money totalAmount;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Order other = (Order) o;
+        if(this.orderNumber == null) return false;
+        return this.orderNumber.equals(other.orderNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((orderNumber == null) ? 0 : orderNumber.hashCode());
+        return result;
+    }
 
     public Order(List<OrderLine> orderLines, ShippingInfo shippingInfo) {
         setOrderLines(orderLines);
